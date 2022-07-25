4394ebebad174f048493443987fe2a32b2de0c66
QuadraKOs <mconstantinou923@gmail.com>
2022-07-26 02:46:55 +0300

    heroku port

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 
 declare const module: any;
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(PORT);
 
   if (module.hot) {
     module.hot.accept();
